b4655cf3e75f56262aa316b0fd60a4bd8d9b553c
Bruno Cesar de Andrade Steckelberg <bruno.steckelberg@dataprev.gov.br>
2021-05-06 16:53:22 -0300

    corrigido a ordem de exibição dos forms de cadastro.

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
 
+import { Typography } from '@material-ui/core';
+
+
 function FormularioCadastro( {aoEnviar, validarCPFProps} ){
+
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  function formularioAtual(etapa){
+
+    switch (etapa) {
+      case 0:
+        return (
+          <DadosUsuario />
+        );
+      case 1:
+        return (
+          <DadosPessoais aoEnviar={aoEnviar} validarCPFProps2={validarCPFProps}/>
+        );
+      case 2:
+        return(
+          <DadosEntrega />
+        );
+      break;
+      default:
+      return(
+        <Typography>Erro ao selecionar renderização</Typography>
+      );
+    }
+  }
 
   return (
     <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPFProps2={validarCPFProps}/>
-      <DadosUsuario />
-      <DadosEntrega />
+      {formularioAtual(etapaAtual)}
     </>
   );
 }
